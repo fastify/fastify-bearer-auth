fadf4b430ed8ee68cfaee8aed6ce01667450d5c6
Iván Aponte <ivan.aponte@gmail.com>
2019-05-10 15:07:15 -0400

    comment Promise.resolve block

--- plugin.js
+++ plugin.js
@@ -30,18 +30,29 @@ function factory (options) {
 
     const key = header.substring(bearerType.length).trim()
     let retVal
+    // check if auth function is defined
     if (auth && auth instanceof Function) {
       try {
         retVal = auth(key)
+      // catch any error from the user provided function
       } catch (err) {
         retVal = Promise.reject(err)
       }
     } else {
+    // if auth is not defined use keys
       retVal = authenticate(keys, key)
     }
 
     const invalidKeyError = Error('invalid authorization header')
+
+    // retVal contains the result of the auth function if defined or the
+    // result of the key comparison.
+    // retVal is enclosed in a Promise.resolve to allow auth to be a normal
+    // function or an async funtion. If it returns a non-promise value it
+    // will be converted to a resolving promise. If it returns a promise it
+    // will be resolved.
     Promise.resolve(retVal).then((val) => {
+      // if val is not truthy return 401
       if (!val) {
         fastifyReq.log.error('invalid authorization header: `%s`', header)
         if (contentType) fastifyRes.header('content-type', contentType)
@@ -51,6 +62,7 @@ function factory (options) {
       next()
     }).catch((err) => {
       if (failSilent) {
+        // if failsilent is set then we return 401
         fastifyRes.code(401).send(errorResponse(invalidKeyError))
       } else {
         fastifyRes.code(500).send(errorResponse(err instanceof Error ? err : Error(String(err))))
