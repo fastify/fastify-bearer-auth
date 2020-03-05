36790c729709631930761e1dce76ce888d4d9c7e
Manuel Spigolon <behemoth89@gmail.com>
2020-03-05 21:35:22 +0100

    removes `request` deprecated dependency (#36)

--- test/integration.test.js
+++ test/integration.test.js
@@ -3,57 +3,52 @@
 const tap = require('tap')
 const test = tap.test
 const fastify = require('fastify')()
-const request = require('request')
 const plugin = require('../')
 
 fastify.register(plugin, { keys: new Set(['123456']) })
 
-fastify.get('/test1', (req, res) => {
+fastify.get('/test', (req, res) => {
   res.send({ hello: 'world' })
 })
 
-fastify.get('/test2', (req, res) => {
-  res.send({ hello: 'world' })
-})
-
-fastify.get('/test3', (req, res) => {
-  res.send({ hello: 'world' })
-})
-
-fastify.listen(0, (err) => {
-  if (err) tap.error(err)
-  fastify.server.unref()
-
-  const reqOpts = {
+test('success route succeeds', (t) => {
+  t.plan(2)
+  fastify.inject({
     method: 'GET',
-    baseUrl: 'http://localhost:' + fastify.server.address().port
-  }
-  const req = request.defaults(reqOpts)
-
-  test('success route succeeds', (t) => {
-    t.plan(3)
-    req({ uri: '/test1', auth: { bearer: '123456' } }, (err, response, body) => {
-      t.error(err)
-      t.strictEqual(response.statusCode, 200)
-      t.deepEqual(JSON.parse(body), { hello: 'world' })
-    })
+    url: '/test',
+    headers: {
+      authorization: 'Bearer 123456'
+    }
+  }).then(response => {
+    t.strictEqual(response.statusCode, 200)
+    t.deepEqual(JSON.parse(response.body), { hello: 'world' })
+  }).catch(err => {
+    t.error(err)
   })
+})
 
-  test('invalid key route fails correctly', (t) => {
-    t.plan(3)
-    req({ uri: '/test2', auth: { bearer: '654321' } }, (err, response, body) => {
-      t.error(err)
-      t.strictEqual(response.statusCode, 401)
-      t.match(JSON.parse(body).error, /invalid authorization header/)
-    })
+test('invalid key route fails correctly', (t) => {
+  t.plan(2)
+  fastify.inject({
+    method: 'GET',
+    url: '/test',
+    headers: {
+      authorization: 'Bearer 987654'
+    }
+  }).then(response => {
+    t.strictEqual(response.statusCode, 401)
+    t.match(JSON.parse(response.body).error, /invalid authorization header/)
+  }).catch(err => {
+    t.error(err)
   })
+})
 
-  test('missing header route fails correctly', (t) => {
-    t.plan(3)
-    req({ uri: '/test3' }, (err, response, body) => {
-      t.error(err)
-      t.strictEqual(response.statusCode, 401)
-      t.match(JSON.parse(body).error, /missing authorization header/)
-    })
+test('missing header route fails correctly', (t) => {
+  t.plan(2)
+  fastify.inject({ method: 'GET', url: '/test' }).then(response => {
+    t.strictEqual(response.statusCode, 401)
+    t.match(JSON.parse(response.body).error, /missing authorization header/)
+  }).catch(err => {
+    t.error(err)
   })
 })
