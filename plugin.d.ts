0aedcd9d7d7b79182a47c80d86e83a7f7d41f1c9
KaKa <kaka@kakawebsitedemo.com>
2021-02-09 07:13:45 +0800

    style: tab to space

--- plugin.d.ts
+++ plugin.d.ts
@@ -1,14 +1,14 @@
 import * as fastify from 'fastify'
 
 declare namespace fastifyBearerAuth {
-	export interface FastifyBearerAuthOptions {
-		keys: Set<string>,
-		auth?: (key: string, req: fastify.FastifyRequest) => boolean | Promise<boolean>,
-		errorResponse?: (err: Error) => { error: string },
-		contentType?: string,
-		bearerType?: string,
+  export interface FastifyBearerAuthOptions {
+    keys: Set<string>,
+    auth?: (key: string, req: fastify.FastifyRequest) => boolean | Promise<boolean>,
+    errorResponse?: (err: Error) => { error: string },
+    contentType?: string,
+    bearerType?: string,
     addHook?: boolean
-	}
+  }
 }
 
 declare const fastifyBearerAuth: fastify.FastifyPlugin<fastifyBearerAuth.FastifyBearerAuthOptions>
--- test/plugin.test-d.ts
+++ test/plugin.test-d.ts
@@ -3,36 +3,36 @@ import { expectAssignable } from 'tsd'
 import bearerAuth, { FastifyBearerAuthOptions } from '../plugin'
 
 const pluginOptions: FastifyBearerAuthOptions = {
-	keys: new Set(['foo']),
-	auth: (key: string, req: FastifyRequest) => { return true },
-	errorResponse: (err: Error) => { return { error: err.message } },
-	contentType: '',
-	bearerType: '',
+  keys: new Set(['foo']),
+  auth: (key: string, req: FastifyRequest) => { return true },
+  errorResponse: (err: Error) => { return { error: err.message } },
+  contentType: '',
+  bearerType: '',
   addHook: false
 }
 
 const pluginOptionsAuthPromise: FastifyBearerAuthOptions = {
-	keys: new Set(['foo']),
-	auth: (key: string, req: FastifyRequest) => { return Promise.resolve(true) },
-	errorResponse: (err: Error) => { return { error: err.message } },
-	contentType: '',
-	bearerType: ''
+  keys: new Set(['foo']),
+  auth: (key: string, req: FastifyRequest) => { return Promise.resolve(true) },
+  errorResponse: (err: Error) => { return { error: err.message } },
+  contentType: '',
+  bearerType: ''
 }
 
 expectAssignable<{
-	keys: Set<string>,
-	auth?: (key: string, req: FastifyRequest) => boolean | Promise<boolean>,
-	errorResponse?: (err: Error) => { error: string },
-	contentType?: string,
-	bearerType?: string
+  keys: Set<string>,
+  auth?: (key: string, req: FastifyRequest) => boolean | Promise<boolean>,
+  errorResponse?: (err: Error) => { error: string },
+  contentType?: string,
+  bearerType?: string
 }>(pluginOptions)
 
 expectAssignable<{
-	keys: Set<string>,
-	auth?: (key: string, req: FastifyRequest) => boolean | Promise<boolean>,
-	errorResponse?: (err: Error) => { error: string },
-	contentType?: string,
-	bearerType?: string
+  keys: Set<string>,
+  auth?: (key: string, req: FastifyRequest) => boolean | Promise<boolean>,
+  errorResponse?: (err: Error) => { error: string },
+  contentType?: string,
+  bearerType?: string
 }>(pluginOptionsAuthPromise)
 
 fastify().register(bearerAuth, pluginOptions)
