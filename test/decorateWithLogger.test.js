04b1383249098a65780ef82e71466f7a2618ca97
songpr <songpr@gmail.com>
2022-02-02 20:00:18 +0700

    Update test/decorateWithLogger.test.js
    Co-authored-by: darkgl0w <31093081+darkgl0w@users.noreply.github.com>

--- test/decorateWithLogger.test.js
+++ test/decorateWithLogger.test.js
@@ -21,29 +21,58 @@ test('verifyBearerAuthFactory', (t) => {
   })
 })
 
-const fastifyLogDebug = require('fastify')({ logger: { level: 'debug' } })
-fastifyLogDebug.register(plugin, { addHook: false, keys: new Set(['123456']), verifyErrorLogLevel: 'debug' })
+test('verifyBearerAuth with debug log', async (t) => {
+  t.plan(5)
 
-test('verifyBearerAuth with debug log', (t) => {
-  t.plan(1)
-  fastifyLogDebug.ready(() => {
-    t.ok(fastifyLogDebug.verifyBearerAuth)
+  const logs = []
+  const destination = new stream.Writable({
+    write: function (chunk, encoding, next) {
+      logs.push(JSON.parse(chunk))
+      next()
+    }
   })
+
+  const fastify = Fastify({ logger: { level: 'debug', stream: destination } })
+  await fastify.register(plugin, { addHook: false, keys: new Set(['123456']), verifyErrorLogLevel: 'debug' })
+
+  fastify.get('/', {
+    onRequest: [
+      fastify.verifyBearerAuth
+    ]
+  }, async (request, reply) => {
+    return { message: 'ok' }
+  })
+
+  await fastify.ready()
+
+  t.ok(fastify.verifyBearerAuth)
+  t.ok(fastify.verifyBearerAuthFactory)
+
+  const response = await fastify.inject({
+    method: 'GET',
+    path: '/',
+    headers: {
+      authorization: 'Bearer bad key'
+    }
+  })
+
+  // Debug level is equal to 20 so we search for an entry with a level of 20
+  const failure = logs.find((entry) => entry.level && entry.level === 20)
+
+  t.equal(failure.level, 20)
+  t.equal(failure.msg, 'unauthorized: invalid authorization header')
+
+  t.equal(response.statusCode, 401)
 })
 
-test('verifyBearerAuthFactory with debug log', (t) => {
-  t.plan(1)
-  fastifyLogDebug.ready(() => {
-    t.ok(fastifyLogDebug.verifyBearerAuthFactory)
-  })
-})
-
-const fastifyLogError = require('fastify')({ logger: { level: 'error' } })
 test('register with invalid log level', async (t) => {
-  const invalidLogLevel = 'invalid'
   t.plan(1)
+
+  const invalidLogLevel = 'invalid'
+  const fastify = Fastify({ logger: { level: 'error' } })
+
   try {
-    await fastifyLogError.register(plugin, { addHook: false, keys: new Set(['123456']), verifyErrorLogLevel: invalidLogLevel })
+    await fastify.register(plugin, { addHook: false, keys: new Set(['123456']), verifyErrorLogLevel: invalidLogLevel })
   } catch (err) {
     t.equal(err.message, `fastify.log does not have level '${invalidLogLevel}'`)
   }
