d505c159276463c654bbc7559186c394cbe1c1b9
darkgl0w <31093081+darkgl0w@users.noreply.github.com>
2022-02-02 14:06:39 +0100

    fix: Update test/decorateWithLogger.test.js

--- test/decorateWithLogger.test.js
+++ test/decorateWithLogger.test.js
@@ -1,25 +1,8 @@
 'use strict'
 
-const tap = require('tap')
-const test = tap.test
-const fastify = require('fastify')({ logger: true })
-const plugin = require('../')
-
-fastify.register(plugin, { addHook: false, keys: new Set(['123456']) })
-
-test('verifyBearerAuth', (t) => {
-  t.plan(1)
-  fastify.ready(() => {
-    t.ok(fastify.verifyBearerAuth)
-  })
-})
-
-test('verifyBearerAuthFactory', (t) => {
-  t.plan(1)
-  fastify.ready(() => {
-    t.ok(fastify.verifyBearerAuthFactory)
-  })
-})
+const { test } = require('tap')
+const Fastify = require('fastify')
+const plugin = require('..')
 
 test('verifyBearerAuth with debug log', async (t) => {
   t.plan(5)
