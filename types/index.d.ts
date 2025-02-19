6ed7f06d14d107b0362cd528ffc5d5acf430a508
Frazer Smith <frazer.dev@icloud.com>
2025-02-19 09:01:43 +0000

    types(index): export errors (#210)
    * types(index): export errors
    
    * types(index): add new hook error

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,6 +3,17 @@ import {
   FastifyReply,
   FastifyPluginCallback
 } from 'fastify'
+import { FastifyError } from '@fastify/error'
+
+declare interface FastifyBearerAuthErrors {
+  FST_BEARER_AUTH_INVALID_KEYS_OPTION_TYPE: FastifyError
+  FST_BEARER_AUTH_INVALID_HOOK: FastifyError
+  FST_BEARER_AUTH_INVALID_LOG_LEVEL: FastifyError
+  FST_BEARER_AUTH_KEYS_OPTION_INVALID_KEY_TYPE: FastifyError
+  FST_BEARER_AUTH_INVALID_SPEC: FastifyError
+  FST_BEARER_AUTH_MISSING_AUTHORIZATION_HEADER: FastifyError
+  FST_BEARER_AUTH_INVALID_AUTHORIZATION_HEADER: FastifyError
+}
 
 declare module 'fastify' {
   interface FastifyInstance {
@@ -29,6 +40,7 @@ declare namespace fastifyBearerAuth {
   export type verifyBearerAuthFactory = (options: fastifyBearerAuth.FastifyBearerAuthOptions) => verifyBearerAuth
 
   export const fastifyBearerAuth: FastifyBearerAuth
+  export const FastifyBearerAuthErrors: FastifyBearerAuthErrors
   export { fastifyBearerAuth as default }
 }
 
